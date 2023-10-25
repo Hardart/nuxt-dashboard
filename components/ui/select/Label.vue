3db37cb011a46c1caf148e2cd9619fa54476163f
Hardart <kaino@bk.ru>
2023-10-25 17:07:48 +0300

    remove old Select component

--- components/ui/select/Label.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-defineProps(['label'])
-</script>
-<template>
-  <HListboxLabel class="block text-sm font-medium leading-6 text-gray-900">{{ label }}</HListboxLabel>
-</template>
